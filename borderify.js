81b3f5d57559e4e6244500990a9d416f93eceb92
Simbayyy <simon.baillet.gomez@gmail.com>
2023-08-24 18:31:02 +0200

    feat: added button action

--- borderify.js
+++ borderify.js
@@ -1,1 +1,28 @@
-document.body.style.border = "5px solid red";
+const APPLICABLE_PROTOCOLS = ["http:", "https:"];
+
+function protocolIsApplicable(url) {
+    const protocol = (new URL(url)).protocol;
+    return APPLICABLE_PROTOCOLS.includes(protocol);
+  }
+
+function initializePageAction(tab) {
+  if (protocolIsApplicable(tab.url)) {
+    browser.pageAction.setIcon({tabId: tab.id, path: "icons/icon-32.png"});
+    browser.pageAction.show(tab.id);
+  }
+}
+
+let gettingAllTabs = browser.tabs.query({});
+gettingAllTabs.then((tabs) => {
+  for (let tab of tabs) {
+    initializePageAction(tab);
+  }
+});
+
+browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
+    initializePageAction(tab);
+  });
+
+browser.pageAction.onClicked.addListener(() => {
+    browser.tabs.executeScript({file: 'borderify-script.js'})
+});
